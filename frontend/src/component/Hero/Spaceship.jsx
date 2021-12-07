6878b22ce3a02d7f4bdbc80787c966cc690294cc
nelakell <31990102+nelakell@users.noreply.github.com>
2021-12-07 18:05:22 +0100

    Update Spaceship.jsx

--- frontend/src/component/Hero/Spaceship.jsx
+++ frontend/src/component/Hero/Spaceship.jsx
@@ -1,13 +1,29 @@
-import playerImg from '../../res/kenney/PNG/playerShip1_red.png';
+import redFighter from '../../res/kenney/PNG/playerShip1_red.png';
+import blueFighter from '../../res/kenney/PNG/playerShip1_blue.png';
+import greenFighter from '../../res/kenney/PNG/playerShip1_green.png';
 import './Hero.css'
 
 
+const playerImgFighter = props => {
+    let fighterColour = props.fighter;
+    if (fighterColour=="red") {
+        return redFighter;
+    }
+    else if (fighterColour=="blue") {
+        return blueFighter
+    }
+    else if (fighterColour=="green"){
+        return greenFighter;
+    }
+};
+
+
 const Spaceship = (props) => {
-    console.log("spaceship")
-    console.log(props)
+    console.log("spaceship");
+    console.log(props);
     return (
         <div className={'spaceship'}>
-            <img className={'spaceshipImg'} src={playerImg} alt={'spaceship'}
+            <img className={'spaceshipImg'} src={playerImgFighter(props)} alt={'spaceship'}
                  style={{
                      position: 'relative',
                      top: props.yPos,
@@ -15,6 +31,6 @@ const Spaceship = (props) => {
                  }}/>
         </div>
     );
-}
+};
 
 export default Spaceship;
